ac3def79e98506d62d51db11ee252077b1a39d9b
Naveen2003Raj <145975706+Naveen2003Raj@users.noreply.github.com>
2024-08-12 21:51:59 +0530

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,6 @@
+background:linear-gradient(-120deg,#c3092D,#7400C3,#222221);
+  background:linear-gradient(-120deg,#E8543E,#5D2530,#BB6C44,#B85C54);
+  background:linear-gradient(45deg,#939F3D,#F3A933,#702800,#AA4E36,#DCDBD6)'p'
+  background:linear-gradient(#c3092D,#7400C3,#222221); h1
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;  
